5e2aa9580acf67f3ab3ecfe48383deee7b890a79
Alexander Diebler <alexander.diebler@barzahlen.de>
2012-05-23 13:06:20 +0200

    - added notifications (TDD) closes #478

--- notification.php
+++ notification.php
@@ -20,6 +20,252 @@
  */
 
 class Barzahlen_Notification extends Barzahlen_Base {
-  
+
+  protected $_isValid = false; //!< state of validity
+  protected $_shopId; //!< merchants shop id
+  protected $_notificationKey; //!< merchants notification key
+  protected $_receivedData; //!< data which were send by Barzahlen
+  protected $_notificationType = 'payment'; //!< type of notification (payment or refund)
+
+  protected $_notficationData = array('state','transaction_id','shop_id','customer_email','amount',
+                                      'currency','order_id','custom_var_0','custom_var_1',
+                                      'custom_var_2','hash'); //!< all necessary attributes for a valid notification
+
+  protected $_originData = array('transaction_id','order_id'); //!< numeric values for database queries
+
+  /**
+   * Constructor. Sets basic settings.
+   *
+   * @param string $shopId merchants shop id
+   * @param string $notificationKey merchants notification key
+   */
+  public function __construct($shopId, $notificationKey, array $receivedData) {
+
+    $this->_shopId = $shopId;
+    $this->_notificationKey = $notificationKey;
+    $this->_receivedData = $receivedData;
+
+    $this->_checkExistence();
+    $this->_checkValues();
+    $this->_checkHash();
+    $this->_isValid = true;
+  }
+
+  /**
+   * Gets state of validity.
+   *
+   * @return boolean if notification is valid
+   */
+  public function isValid() {
+    return $this->_isValid;
+  }
+
+  /**
+   * Checks that all attributes are available.
+   */
+  protected function _checkExistence() {
+
+    if(array_key_exists('refund_transaction_id', $this->_receivedData)) {
+      $this->_notificationType = 'refund';
+      foreach ($this->_originData as $attribute) {
+        $this->_notficationData = str_replace($attribute, 'origin_' . $attribute, $this->_notficationData);
+      }
+    }
+
+    foreach ($this->_notficationData as $attribute) {
+      if(!array_key_exists($attribute, $this->_receivedData)) {
+        throw new Barzahlen_Exception('Notification array not complete, at least '. $attribute .' is missing.');
+      }
+    }
+  }
+
+  /**
+   * Checks that all numeric attributes are numeric.
+   */
+  protected function _checkValues() {
+
+    $invalid = 0;
+
+    if($this->_notificationType == 'refund') {
+      $invalid += !is_numeric($this->_receivedData['refund_transaction_id']);
+      $invalid += !is_numeric($this->_receivedData['origin_transaction_id']);
+      $invalid += !is_numeric($this->_receivedData['origin_order_id']);
+    }
+    else {
+      $invalid += !is_numeric($this->_receivedData['transaction_id']);
+      $invalid += !is_numeric($this->_receivedData['order_id']);
+    }
+
+    $invalid += $this->_shopId != $this->_receivedData['shop_id'];
+    $invalid += !preg_match('/^(1000(\.00?)?|\d{1,3}(\.\d\d?)?)$/', $this->_receivedData['amount']);
+
+    if($invalid > 0) {
+      throw new Barzahlen_Exception('Notification contains '. $invalid .' invalid values.');
+    }
+  }
+
+  /**
+   * Checks that received hash is valid.
+   */
+  protected function _checkHash() {
+
+    $receivedHash = $this->_receivedData['hash'];
+    unset($this->_receivedData['hash']);
+    $generatedHash = $this->_createHash($this->_receivedData, $this->_notificationKey);
+
+    if($receivedHash != $generatedHash){
+      throw new Barzahlen_Exception('Notification hash is not valid.');
+    }
+  }
+
+  /**
+   * Returns a single value from the notification array or the whole array.
+   *
+   * @param string $attribute single attribute, that shall be returned
+   * @return single value if exists (else: null) or whole array
+   */
+  public function getNotificationArray($attribute = '') {
+
+    if($attribute != '') {
+      return array_key_exists($attribute, $this->_receivedData) && $this->_isValid ? $this->_receivedData[$attribute] : null;
+    }
+
+    return $this->_isValid ? $this->_receivedData : null;
+  }
+
+  /**
+   * Returns notification type.
+   *
+   * @return string with notification type
+   */
+  public function getNotificationType() {
+    return $this->_isValid ? $this->_notificationType : null;
+  }
+
+  /**
+   * Returns notification state.
+   *
+   * @return string with state
+   */
+  public function getState() {
+    return $this->getNotificationArray('state');
+  }
+
+  /**
+   * Returns refund transaction id.
+   *
+   * @return string with refund transaction id
+   */
+  public function getRefundTransactionId() {
+    return $this->getNotificationArray('refund_transaction_id');
+  }
+
+  /**
+   * Returns transaction id.
+   *
+   * @return string with transaction id
+   */
+  public function getTransactionId() {
+    return $this->getNotificationArray('transaction_id');
+  }
+
+  /**
+   * Returns origin transaction id.
+   *
+   * @return string with origin transaction id
+   */
+  public function getOriginTransactionId() {
+    return $this->getNotificationArray('origin_transaction_id');
+  }
+
+  /**
+   * Returns shop id.
+   *
+   * @return string with shop id
+   */
+  public function getShopId() {
+    return $this->getNotificationArray('shop_id');
+  }
+
+  /**
+   * Returns customer e-mail.
+   *
+   * @return string with customer e-mail
+   */
+  public function getCustomerEmail() {
+    return $this->getNotificationArray('customer_email');
+  }
+
+  /**
+   * Returns amount.
+   *
+   * @return string with amount
+   */
+  public function getAmount() {
+    return $this->getNotificationArray('amount');
+  }
+
+  /**
+   * Returns currency.
+   *
+   * @return string with currency
+   */
+  public function getCurrency() {
+    return $this->getNotificationArray('currency');
+  }
+
+  /**
+   * Returns order id.
+   *
+   * @return string with order id
+   */
+  public function getOrderId() {
+    return $this->getNotificationArray('order_id');
+  }
+
+  /**
+   * Returns origin order id.
+   *
+   * @return string with origin order id
+   */
+  public function getOriginOrderId() {
+    return $this->getNotificationArray('origin_order_id');
+  }
+
+  /**
+   * Returns customer var 1.
+   *
+   * @return string with custom var
+   */
+  public function getCustomVar1() {
+    return $this->getNotificationArray('custom_var_0');
+  }
+
+  /**
+   * Returns customer var 2.
+   *
+   * @return string with custom var
+   */
+  public function getCustomVar2() {
+    return $this->getNotificationArray('custom_var_1');
+  }
+
+  /**
+   * Returns customer var 3.
+   *
+   * @return string with custom var
+   */
+  public function getCustomVar3() {
+    return $this->getNotificationArray('custom_var_2');
+  }
+
+  /**
+   * Returns customer var as array.
+   *
+   * @return array with custom variables
+   */
+  public function getCustomVar() {
+    return array($this->getCustomVar1(), $this->getCustomVar2(), $this->getCustomVar3());
+  }
 }
 ?>
---
+++ tests/sdk/NotificationTest.php
@@ -0,0 +1,213 @@
+<?php
+/**
+ * Bar zahlen Payment Module
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@barzahlen.de so we can send you a copy immediately.
+ *
+ * @category    ZerebroInternet
+ * @package     ZerebroInternet_Barzahlen
+ * @copyright   Copyright (c) 2012 Zerebro Internet GmbH (http://www.barzahlen.de)
+ * @author      Alexander Diebler (alexander.diebler@barzahlen.de)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL-3.0)
+ */
+
+class NotificationTest extends PHPUnit_Framework_TestCase {
+
+  /**
+   * Set everything that is needed for the testing up.
+   */
+  public function setUp() {
+  }
+
+  /**
+   * Test that empty arrays are decleared not valid.
+   *
+   * @expectedException Barzahlen_Exception
+   */
+  public function testEmptyNotification() {
+
+    $_GET = array();
+
+    $this->notification = new Barzahlen_Notification(SHOPID, NOTIFICATIONKEY, $_GET);
+  }
+
+  /**
+   * Test that incomplete arrays are decleared not valid.
+   *
+   * @expectedException Barzahlen_Exception
+   */
+  public function testIncompleteNotification() {
+
+    $_GET = array('state' => 'paid',
+                  'transaction_id' => '5');
+
+    $this->notification = new Barzahlen_Notification(SHOPID, NOTIFICATIONKEY, $_GET);
+  }
+
+  /**
+   * Test function with invalid values.
+   *
+   * @expectedException Barzahlen_Exception
+   */
+  public function testInvalidValuesNotification() {
+
+   $_GET = array('state' => 'paid',
+                 'transaction_id' => '<hack>',
+                 'shop_id' => '10483',
+                 'customer_email' => 'foo@bar.com',
+                 'amount' => '24.95',
+                 'currency' => 'EUR',
+                 'order_id' => '1',
+                 'custom_var_0' => 'PHP SDK',
+                 'custom_var_1' => 'Euro 2012',
+                 'custom_var_2' => 'Barzahlen v.1.3.3.7',
+                 'hash' => 'fb4393c37919371968f786a174b5a9b7340bc7e397fc480dd0d81e97873f87303c6799e855bc0a36d8673957bf00392b5b9a23f772660e67719534f13ac6d5c1'
+                   );
+
+    $this->notification = new Barzahlen_Notification(SHOPID, NOTIFICATIONKEY, $_GET);
+  }
+
+  /**
+   * Test function with amount values.
+   *
+   * @expectedException Barzahlen_Exception
+   */
+  public function testInvalidAmountNotification() {
+
+   $_GET = array('state' => 'paid',
+                 'transaction_id' => '2004.95',
+                 'shop_id' => '10483',
+                 'customer_email' => 'foo@bar.com',
+                 'amount' => '24.95',
+                 'currency' => 'EUR',
+                 'order_id' => '1',
+                 'custom_var_0' => 'PHP SDK',
+                 'custom_var_1' => 'Euro 2012',
+                 'custom_var_2' => 'Barzahlen v.1.3.3.7',
+                 'hash' => 'f37e091346df8f8a9dfed61772d62d1dae22bd30e159836fa1c01f21c4ce2933c0153fe66e8629601c695c3b28a6d61f20f1bfa3d66e54c362637b432e3dc265'
+                   );
+
+    $this->notification = new Barzahlen_Notification(SHOPID, NOTIFICATIONKEY, $_GET);
+  }
+
+  /**
+   * Test function with invalid hash paid notification.
+   *
+   * @expectedException Barzahlen_Exception
+   */
+  public function testInvalidHashPaidNotification() {
+
+   $_GET = array('state' => 'paid',
+                 'transaction_id' => '1',
+                 'shop_id' => '10483',
+                 'customer_email' => 'foo@bar.com',
+                 'amount' => '24.95',
+                 'currency' => 'EUR',
+                 'order_id' => '1',
+                 'custom_var_0' => 'PHP SDK',
+                 'custom_var_1' => 'Euro 2012',
+                 'custom_var_2' => 'Barzahlen v.1.3.3.7',
+                 'hash' => '85d13e7eda95276a655ef86947409f095be8ccd1736579d54a88fc9ce2ac5353964b33d8143439354ee46fa3ce0a7ea07c49429ae3bdbfeca4f2ab1990c15367'
+                   );
+
+    $this->notification = new Barzahlen_Notification(SHOPID, NOTIFICATIONKEY, $_GET);
+  }
+
+  /**
+   * Test function with valid paid notification.
+   */
+  public function testValidPaidNotification() {
+
+   $_GET = array('state' => 'paid',
+                 'transaction_id' => '1',
+                 'shop_id' => '10483',
+                 'customer_email' => 'foo@bar.com',
+                 'amount' => '24.95',
+                 'currency' => 'EUR',
+                 'order_id' => '1',
+                 'custom_var_0' => 'PHP SDK',
+                 'custom_var_1' => 'Euro 2012',
+                 'custom_var_2' => 'Barzahlen v.1.3.3.7',
+                 'hash' => '85d13e7eda95276a655ef86947409f095be8ccd1736579d54a88fc9ce2ac5353964b33d8143439354ee46fa3ce0a7ea07c49429ae3bdbfeca4f2ab1990c15366'
+                   );
+
+    $this->notification = new Barzahlen_Notification(SHOPID, NOTIFICATIONKEY, $_GET);
+
+    $this->assertTrue($this->notification->isValid());
+    $this->assertEquals('payment', $this->notification->getNotificationType());
+    $this->assertEquals('paid', $this->notification->getState());
+    $this->assertEquals('1', $this->notification->getTransactionId());
+    $this->assertEquals('10483', $this->notification->getShopId());
+    $this->assertEquals('foo@bar.com', $this->notification->getCustomerEmail());
+    $this->assertEquals('24.95', $this->notification->getAmount());
+    $this->assertEquals('EUR', $this->notification->getCurrency());
+    $this->assertEquals('1', $this->notification->getOrderId());
+    $this->assertEquals('PHP SDK', $this->notification->getCustomVar1());
+    $this->assertEquals('Euro 2012', $this->notification->getCustomVar2());
+    $this->assertEquals('Barzahlen v.1.3.3.7', $this->notification->getCustomVar3());
+    $this->assertEquals(array('PHP SDK', 'Euro 2012', 'Barzahlen v.1.3.3.7'), $this->notification->getCustomVar());
+
+    unset($_GET['hash']);
+    $this->assertEquals($_GET, $this->notification->getNotificationArray());
+  }
+
+  /**
+   * Test function with valid refund notification.
+   */
+  public function testValidRefundNotification() {
+
+   $_GET = array('state' => 'refund_completed',
+                 'refund_transaction_id' => '1',
+                 'origin_transaction_id' => '1',
+                 'shop_id' => '10483',
+                 'customer_email' => 'foo@bar.com',
+                 'amount' => '24.95',
+                 'currency' => 'EUR',
+                 'origin_order_id' => '1',
+                 'custom_var_0' => 'PHP SDK',
+                 'custom_var_1' => 'Euro 2012',
+                 'custom_var_2' => 'Barzahlen v.1.3.3.7',
+                 'hash' => '55b3b182caf79881f5ac9a4fd7ac4f84824267fc8ac8a18dcfd25535b48a646eb28a0acf864faaff006365fd5f0480c09341930bf15dbcbe3ad27e4fa0d5c9f5'
+                   );
+
+    $this->notification = new Barzahlen_Notification(SHOPID, NOTIFICATIONKEY, $_GET);
+
+    $this->assertTrue($this->notification->isValid());
+    $this->assertEquals('refund', $this->notification->getNotificationType());
+    $this->assertEquals('refund_completed', $this->notification->getState());
+    $this->assertEquals('1', $this->notification->getRefundTransactionId());
+    $this->assertEquals(null, $this->notification->getTransactionId());
+    $this->assertEquals('1', $this->notification->getOriginTransactionId());
+    $this->assertEquals('10483', $this->notification->getShopId());
+    $this->assertEquals('foo@bar.com', $this->notification->getCustomerEmail());
+    $this->assertEquals('24.95', $this->notification->getAmount());
+    $this->assertEquals('EUR', $this->notification->getCurrency());
+    $this->assertEquals(null, $this->notification->getOrderId());
+    $this->assertEquals('1', $this->notification->getOriginOrderId());
+    $this->assertEquals('PHP SDK', $this->notification->getCustomVar1());
+    $this->assertEquals('Euro 2012', $this->notification->getCustomVar2());
+    $this->assertEquals('Barzahlen v.1.3.3.7', $this->notification->getCustomVar3());
+    $this->assertEquals(array('PHP SDK', 'Euro 2012', 'Barzahlen v.1.3.3.7'), $this->notification->getCustomVar());
+
+    unset($_GET['hash']);
+    $this->assertEquals($_GET, $this->notification->getNotificationArray());
+  }
+
+  /**
+   * Unset everything before the next test.
+   */
+  protected function tearDown() {
+
+    unset($this->notification);
+  }
+}
+?>
+  
