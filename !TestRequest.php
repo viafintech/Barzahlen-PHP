557b1fa10540818c4931532c3df86e5dac03e4f4
Alexander Diebler <alexander.diebler@barzahlen.de>
2012-11-02 10:02:08 +0100

    - fixed typos in doxygen main page

--- !TestRequest.php
+++ !TestRequest.php
@@ -2,18 +2,18 @@
 require_once('loader.php');
 
 $api = new Barzahlen_Api('10483', '22b8231bcf3a47e81fc1c7ec19f07fb8c10e94e8');
-$payment = new Barzahlen_Request_Payment('foo@bar.com', 'Fabeckstr. 15', '14195', 'Berlin', 'DE', '45.99');
-$update = new Barzahlen_Request_Update('21162918', '66');
+$payment = new Barzahlen_Request_Payment('mustermann@barzahlen.de', 'Fabeckstr. 15', '14195', 'Berlin', 'DE', '45.99');
+$update = new Barzahlen_Request_Update('21846231', '66');
 $refund = new Barzahlen_Request_Refund('21070102', '24.95');
-$resend = new Barzahlen_Request_Resend('7766189');
+$resend = new Barzahlen_Request_Resend('21846833');
 
 try {
-  $api->handleRequest($update);
+  $api->handleRequest($resend);
 }
 catch (Barzahlen_Exception $e) {
   echo $e;
 }
 
-echo "Payment:\n";
-var_dump($update);
+echo "Request:\n";
+var_dump($resend);
 ?>
