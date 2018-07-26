39b5e7dfe0edc8dc960722a8d461353b3dfca963
Rene Kapusta <rene.kapusta@gmail.com>
2018-07-26 14:11:17 +0200

    fixes problems with a) HTTP_X_FORWARDED_PORT vs SERVER_PORT b) REQUEST_URI with query params

--- src/Webhook.php
+++ src/Webhook.php
@@ -25,10 +25,16 @@ class Webhook
      */
     public function verify($header, $body)
     {
+        if (isset($header['HTTP_X_FORWARDED_HOST'])) {
+            $host = $header['HTTP_X_FORWARDED_HOST'] . ':' . $header['HTTP_X_FORWARDED_PORT'];
+        } else {
+            $host = $header['HTTP_HOST'] . ':' . $header['SERVER_PORT'];
+        }
+
         $signature = Middleware::generateSignature(
-            $header['HTTP_HOST'] . ':' . $header['SERVER_PORT'],
+            $host,
             $header['REQUEST_METHOD'],
-            $header['REQUEST_URI'],
+            strtok($header['REQUEST_URI'], '?'),
             $header['QUERY_STRING'],
             $header['HTTP_DATE'],
             '',
