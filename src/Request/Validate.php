ac135f621d71bddb983bfbf43f7b9837c95c3e9a
Sebastian Rahmel <s.rahmel@encurio.com>
2019-09-13 17:36:11 +0200

    - work on validation

--- src/Request/Validate.php
+++ src/Request/Validate.php
@@ -6,32 +6,103 @@ use Barzahlen\Translate;
 
 class Validate
 {
-    public function checkSlipType()
+    /**
+     * checks slip type
+     *
+     * @param $sSlipType
+     * @return bool
+     */
+    public function checkSlipType($sSlipType)
     {
         //Only payment, payout, refund
+        $aSlipTypes = array('payment', 'payout', 'refund');
+
+        if(in_array($sSlipType, $aSlipTypes)) {
+            return true;
+        }
+
+        return false;
     }
 
 
-    public function checkCustomerKey()
+    /**
+     * checks if customer key is in correct format
+     * @param $sKey
+     * @return bool
+     */
+    public function checkCustomerKey($sKey)
     {
-        //check length and only allowed characters, numbers and special chars
+        //Check length and only allowed characters, numbers and special chars
+        if(mb_strlen($sKey) > 80) {
+            return false;
+        }
+
+        if(!preg_match('^[a-zA-Z0-9!"#$%&\'()*+,-./:;<=>?@\[\\\]\^_\{\|\}~]+$', $sKey))
+        {
+            return false;
+        }
+
+        return true;
     }
 
-    public function checkTransaction($fAmount, $sIso3Currency)
+    /**
+     * @param $fAmount
+     * @param $sIso3Currency
+     * @param bool $throwException
+     * @return bool
+     * @throws ApiException
+     */
+    public function checkTransaction($fAmount, $sIso3Currency, $throwException = false)
     {
+        //amount
+        if(!preg_match('/^[0-9]+\.[0-9]{2}$/', $fAmount)) {
+            if($throwException)
+                throw new ApiException('%s. Not a valid float format for the amount like 13.23', 'N/A', array($fAmount), true);
+            return false;
+        }
 
+        if(!preg_match('/^[a-zA-Z]{3}$/', $sIso3Currency)) {
+            if($throwException)
+                throw new ApiException('%s. Not a valid float format for the currency setting like or EUR, USD', 'N/A', array($fAmount), true);
+            return false;
+        }
+
+        return true;
     }
 
-    public function checkHookUrl()
+    /**
+     * @param $sUrl
+     * @param bool $throwException
+     * @return bool
+     * @throws ApiException
+     */
+    public function checkHookUrl($sUrl, $throwException = false)
     {
         //check if is https://
         //check if valid URL
+        if(!preg_match('%^(?:(?:https)://)(?:\S+(?::\S*)?@|\d{1,3}(?:\.\d{1,3}){3}|(?:(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)(?:\.(?:[a-z\d\x{00a1}-\x{ffff}]+-?)*[a-z\d\x{00a1}-\x{ffff}]+)*(?:\.[a-z\x{00a1}-\x{ffff}]{2,6}))(?::\d+)?(?:[^\s]*)?$%iu', $sUrl)) {
+            if($throwException)
+                throw new ApiException('%s. Not a valid url format. A secure url (https) is essential.', 'N/A', array($sUrl), true);
+            return false;
+        }
+
+        return true;
     }
 
-    public function checkExpiresAt()
+    public function checkExpiresAt($sDate)
     {
         //check if date format is correct 'Y-m-d\TH:i:s\Z'
         //check if is in future
+        try {
+            $oDate = new \DateTime($sDate);
+            if($oDate->format('Y-m-d\TH:i:s\Z') != $sDate) {
+                return false;
+            }
+        } catch (\Exception $e) {
+            return false;
+        }
+
+        return true;
     }
 
     public function checkCustomer(array $aCustomerData)
